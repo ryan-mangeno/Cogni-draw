e765f4c9d7d5a35eb937667a6e675c28c115acea
anakin004 <ryanmangeno@gmail.com>
2025-04-16 12:51:29 -0400

    fixed logical error where I pass in a static string stream as a ref into python async function, but it gets initalized once, and when another async python fuction gets called again, the command becomes invalid, using a string and passing by r val ref

--- Cogni-draw/src/RunPy.cpp
+++ Cogni-draw/src/RunPy.cpp
@@ -6,18 +6,18 @@
 void run_async_python(const std::string& input_file, std::atomic_flag& is_done)
 {
 
-    static std::stringstream sys_cmd("");
+    std::string sys_cmd("");
 
 #ifdef _WIN32
-    sys_cmd << "python " << input_file;
+    sys_cmd = "python " + input_file;
 #elif __linux__
-    sys_cmd << "python3 " << input_file;
+    sys_cmd + "python3 " + input_file;
 #endif       
 
 
-    std::function<void()> run_script_fn = [&] 
+    std::function<void()> run_script_fn = [cmd = std::move(sys_cmd), &is_done]
         {
-            std::system(sys_cmd.str().c_str()); 
+            std::system(cmd.c_str());
             is_done.test_and_set();
         };
 
